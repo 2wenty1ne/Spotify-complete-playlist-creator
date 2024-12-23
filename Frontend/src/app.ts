e31f6cd6a7c28b946b8abfbb9e6b3de7761861f1
2wenty1ne <sirdarkvic@gmail.com>
2024-12-23 21:05:31 +0100

    Frontend: test commit

--- Frontend/src/app.ts
+++ Frontend/src/app.ts
@@ -2,7 +2,7 @@ import { handleErr } from "./err/err";
 import { handleHome } from "./home/home"
 
 //TODO show error message on screen if html element not found instead of console.err
-//TODO -> global error function to send error alert
+//TODO -> global error function to send error alert 
 document.addEventListener('DOMContentLoaded', () => {
     const bodyId = document.body.id;
 
